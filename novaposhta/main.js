bed156cec1e91d337995a7bb5ba1ca7687077fd9
Max <imax.webdev@gmail.com>
2020-06-25 03:22:53 +0300

    Commit

--- novaposhta/main.js
+++ novaposhta/main.js
@@ -1,4 +1,4 @@
-let requestURL = 'http://api.odesseo.com.ua/warehouses';
+let requestURL = 'https://api.odesseo.com.ua/warehouses';
 let request = new XMLHttpRequest();
 
 request.open('GET', requestURL, true);
